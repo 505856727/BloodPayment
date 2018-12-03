1cf65d33083fa6af5420d1b59590b4b15112829e
505856727 <30371192+505856727@users.noreply.github.com>
2018-12-03 21:35:07 +0800

    小女孩行走重置，第二关修改

--- Assets/Scripts/Character/Human_PlayerController.cs
+++ Assets/Scripts/Character/Human_PlayerController.cs
@@ -33,88 +33,120 @@ public class Human_PlayerController : MonoBehaviour
 
     public AudioClip[] jumpClips;           // Array of clips for when the player jumps.
     public AudioClip[] switchClips;           // Array of clips for when the player jumps.
-    // Use this for initialization
-    void Start()
-    {
-        //初始化阳光触发时间
-        m_anim = GetComponent<Animator>();
-        // Setting up references.
-        groundCheck = transform.Find("groundCheck");
-        anim = GetComponent<Animator>();
-    }
+                                              // Use this for initialization
+                                              //void Start()
+                                              //{
+                                              //    //初始化阳光触发时间
+                                              //    m_anim = GetComponent<Animator>();
+                                              //    // Setting up references.
+                                              //    groundCheck = transform.Find("groundCheck");
+                                              //    anim = GetComponent<Animator>();
+                                              //}
 
-    // Update is called once per frame
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+    //    grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+    //    if (isBeingSucked)
+    //        return;
+    //    // If the jump button is pressed and the player is grounded then the player should jump.
+    //    if (Input.GetButtonDown("Jump") && grounded)
+    //        jump = true;
+
+    //    if (Input.GetKeyDown(KeyCode.F) && grounded)
+    //        TurnTrigger();
+    //}
+
+    //void FixedUpdate()
+    //{
+    //    if (isBeingSucked)
+    //        return;
+    //    // Cache the horizontal input.
+    //    float h = Input.GetAxis("Horizontal");
+
+    //    // The Speed animator parameter is set to the absolute value of the horizontal input.
+    //    anim.SetFloat("Speed", Mathf.Abs(h));
+
+    //    // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+    //    if (h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+    //        // ... add a force to the player.
+    //        GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce * vitality / 100.0f);
+
+    //    // If the player's horizontal velocity is greater than the maxSpeed...
+    //    if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+    //        // ... set the player's velocity to the maxSpeed in the x axis.
+    //        GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed * vitality / 100.0f, GetComponent<Rigidbody2D>().velocity.y);
+
+    //    // If the input is moving the player right and the player is facing left...
+    //    if (h > 0 && !facingRight)
+    //        // ... flip the player.
+    //        Flip();
+    //    // Otherwise if the input is moving the player left and the player is facing right...
+    //    else if (h < 0 && facingRight)
+    //        // ... flip the player.
+    //        Flip();
+
+    //    // If the player should jump...
+    //    if (jump)
+    //    {
+    //        // Play a random jump audio clip.
+    //        int i = Random.Range(0, jumpClips.Length);
+    //        //AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
+
+    //        // Add a vertical force to the player.
+    //        GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce * vitality / 100.0f));
+
+    //        //保证只跳了一次
+    //        m_anim.SetTrigger("jump");
+
+    //        // Make sure the player can't jump again until the jump conditions from Update are satisfied.
+    //        jump = false;
+    //    }
+    //}
+
+    //void Flip()
+    //{
+    //    // Switch the way the player is labelled as facing.
+    //    facingRight = !facingRight;
+
+    //    // Multiply the player's x local scale by -1.
+    //    Vector3 theScale = transform.localScale;
+    //    theScale.x *= -1;
+    //    transform.localScale = theScale;
+    //}
+    public float speed;
+    public bool isjump;
+    public float jumpspeed;
     void Update()
     {
-        // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-
-        if (isBeingSucked)
-            return;
-        // If the jump button is pressed and the player is grounded then the player should jump.
-        if (Input.GetButtonDown("Jump") && grounded)
-            jump = true;
-
-        if (Input.GetKeyDown(KeyCode.F) && grounded)
-            TurnTrigger();
+        MoveCharacter();
     }
 
-    void FixedUpdate()
+    void MoveCharacter()
     {
-        if (isBeingSucked)
-            return;
-        // Cache the horizontal input.
-        float h = Input.GetAxis("Horizontal");
-
-        // The Speed animator parameter is set to the absolute value of the horizontal input.
-        anim.SetFloat("Speed", Mathf.Abs(h));
-
-        // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-        if (h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
-            // ... add a force to the player.
-            GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce * vitality / 100.0f);
-
-        // If the player's horizontal velocity is greater than the maxSpeed...
-        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
-            // ... set the player's velocity to the maxSpeed in the x axis.
-            GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed * vitality / 100.0f, GetComponent<Rigidbody2D>().velocity.y);
-
-        // If the input is moving the player right and the player is facing left...
-        if (h > 0 && !facingRight)
-            // ... flip the player.
-            Flip();
-        // Otherwise if the input is moving the player left and the player is facing right...
-        else if (h < 0 && facingRight)
-            // ... flip the player.
-            Flip();
-
-        // If the player should jump...
-        if (jump)
+        if (Input.GetKey(KeyCode.A))
         {
-            // Play a random jump audio clip.
-            int i = Random.Range(0, jumpClips.Length);
-            //AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
-
-            // Add a vertical force to the player.
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce * vitality / 100.0f));
-
-            //保证只跳了一次
-            m_anim.SetTrigger("jump");
-
-            // Make sure the player can't jump again until the jump conditions from Update are satisfied.
-            jump = false;
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isjump == false)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpspeed * Time.deltaTime));
+            isjump = true;
         }
     }
 
-    void Flip()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Switch the way the player is labelled as facing.
-        facingRight = !facingRight;
-
-        // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        if (collision.gameObject.tag == "floor")
+        {
+            isjump = false;
+        }
     }
 
     void TurnTrigger()//关闭或者打开机关
