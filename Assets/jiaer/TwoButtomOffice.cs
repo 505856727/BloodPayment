9a866f16f3ca6a07390cc64ff6d5b5ea97ef34cf
505856727 <30371192+505856727@users.noreply.github.com>
2018-12-03 13:42:09 +0800

    jiaer
    双人踩踏机关

--- Assets/jiaer/TwoButtomOffice.cs
+++ Assets/jiaer/TwoButtomOffice.cs
@@ -6,13 +6,42 @@ public class TwoButtomOffice : MonoBehaviour {
     public GameObject leftbuttom;
     public GameObject rightbuttom;
     public GameObject office;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float officeleft;
+    public float officeright;
+    public bool islast;
+    public bool ismove;
+    public float speed;
+    private void Update()
+    {
+        if (leftbuttom.GetComponent<ButtomDown>().isdown && rightbuttom.GetComponent<ButtomDown>().isdown && !ismove)
+        {
+            StopCoroutine("RightMove");
+            StartCoroutine("LeftMove");
+            ismove = true;
+        }
+        else if((!leftbuttom.GetComponent<ButtomDown>().isdown || !rightbuttom.GetComponent<ButtomDown>().isdown)&& ismove)
+        {
+            StopCoroutine("LeftMove");
+            StartCoroutine("RightMove");
+            ismove = false;
+        }
+    }
+
+    IEnumerator LeftMove()
+    {
+        while (office.transform.position.x < officeright)
+        {
+            office.transform.Translate(speed * Time.deltaTime, 0, 0);
+            yield return null;
+        }
+    }
+
+    IEnumerator RightMove()
+    {
+        while (office.transform.position.x > officeleft)
+        {
+            office.transform.Translate(-speed * Time.deltaTime, 0, 0);
+            yield return null;
+        }
+    }
 }
