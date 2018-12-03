94bf44c87834dc8e8c2c1eee98a2b84152baa779
505856727 <30371192+505856727@users.noreply.github.com>
2018-12-03 12:34:34 +0800

    jiaer

---
+++ Assets/jiaer/ButtomDown.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtomDown : MonoBehaviour {
+    public bool isdown = false;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10 || collision.gameObject.layer == 8)
+        {
+            isdown = true;
+        }
+    }
+}
---
+++ Assets/jiaer/TwoButtomOffice.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoButtomOffice : MonoBehaviour {
+    public GameObject leftbuttom;
+    public GameObject rightbuttom;
+    public GameObject office;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
