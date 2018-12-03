d707f75ff064c5a0b842119c4a74616c25d3de9c
DengJunYuan <540997806@qq.com>
2018-12-03 16:01:13 +0800

    增加窗户的交互，修复角色跳跃bug，需要所有平台的layer改成Ground

--- Assets/Scripts/Mechanism/TurnOn.cs
+++ Assets/Scripts/Mechanism/TurnOn.cs
@@ -6,10 +6,18 @@ public class TurnOn : MonoBehaviour
 {
     public GameObject targetToTurnOn;
     public bool isOn = true;
+    public GameObject OnEnableObject;
+    public GameObject OffEnableObject;
 
     public void turnOn()
     {
         isOn = !isOn;
         targetToTurnOn.SetActive(isOn);
+
+        if(OnEnableObject && OffEnableObject)
+        {
+            OnEnableObject.SetActive(isOn);
+            OffEnableObject.SetActive(!isOn);
+        }
     }
 }
