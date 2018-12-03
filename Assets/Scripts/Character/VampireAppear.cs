6c85b3ddb676a1defbce789b38dcc8803f6cfa7c
505856727 <30371192+505856727@users.noreply.github.com>
2018-12-04 00:15:21 +0800

    小女孩一个人走，突然出现吸血鬼

---
+++ Assets/Scripts/Character/VampireAppear.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VampireAppear : MonoBehaviour {
+    public GameObject vampire;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            vampire.SetActive(true);
+        }
+    }
+}
