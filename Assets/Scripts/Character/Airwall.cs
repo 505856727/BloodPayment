f810a932e10ab85a8fb634aa72a2a0a677074452
505856727 <30371192+505856727@users.noreply.github.com>
2018-12-03 18:07:11 +0800

    jiaer
    终于tmd搭完了

---
+++ Assets/Scripts/Character/Airwall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airwall : MonoBehaviour {
+    public GameObject Camera;
+    public Vector3 offset;
+    private void Start()
+    {
+        offset = transform.position - Camera.transform.position; 
+    }
+    private void Update()
+    {
+        transform.position = Camera.transform.position + offset;
+    }
+}
